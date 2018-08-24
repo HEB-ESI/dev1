724683b35f14a5b857e3855f3cef728d133eeb91
Frédéric Servais <fservais@he2b.be>
2018-08-24 13:27:01 +0200

    labo-java - TD1 solutions

---
+++ labo-java/labo-java-01-introduction/solutions/Exercice5.java
@@ -0,0 +1,14 @@
+package g12345.dev1.td1;
+
+public class Exercice5 {
+    public static void main(String[] args) {
+        int a = 51;
+        int b = 17;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a*a+b*b);
+    }
+}
