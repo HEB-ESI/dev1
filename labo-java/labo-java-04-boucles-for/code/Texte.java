759da546e7d359aec3b4a0368eb97d9f0b3c155b
fservais1 <fservais@he2b.be>
2018-08-27 10:50:42 +0200

    labo-jabva - td4 code boucle for

---
+++ labo-java/labo-java-04-boucles-for/code/Texte.java
@@ -0,0 +1,35 @@
+package esi.dev1.td4;
+
+import java.util.Scanner;
+
+public class Texte {
+
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        String texte = clavier.nextLine(); //lit du texte au clavier 
+        
+        System.out.println(texte);
+        System.out.println("Le texte entré est: "+texte);
+        
+        int longueur = texte.length(); //le nombre de caractères
+        System.out.println("La longueur du texte est: "+longueur); 
+        
+        char premièreLettre = texte.charAt(0);
+        char dernièreLettre = texte.charAt(longueur-1);
+        System.out.println(premièreLettre);
+        System.out.println(dernièreLettre);
+        
+        
+        for(int i=0; i< texte.length(); i=i+1) { // parcours la chaine de caractères
+            System.out.print(texte.charAt(i));
+        }
+        
+        System.out.println(); //passe à la ligne
+        
+        for(int i=texte.length()-1; i>=0; i=i-1) { // parcours la chaine de caractères à l'envers
+            System.out.print(texte.charAt(i));
+        }
+        
+        System.out.println(texte.equals("Bonjour")); //affiche true si le texte entré est "Bonjour"
+    }
+}
