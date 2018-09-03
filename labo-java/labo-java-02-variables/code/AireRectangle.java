f2e39c0b817b2ed5dd91c520fe57fecee0b28083
Frédéric Servais <fservais@he2b.be>
2018-09-03 14:38:20 +0200

    labo-java - TD2 variables

---
+++ labo-java/labo-java-02-variables/code/AireRectangle.java
@@ -0,0 +1,17 @@
+package esi.dev1.td2;
+
+import java.util.Scanner;
+
+public class AireRectangle {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Entrez la longueur du rectangle: ");
+        int longueur = clavier.nextInt();
+        
+        System.out.println("Entrez la largeur du rectangle: ");
+        int largeur = clavier.nextInt();
+        
+        System.out.println("L'aire du rectangle vaut: ");
+        System.out.println(largeur*longueur);
+    }
+}
