8f902fdb0e8c6c4f4408dab3035b35df3eab2336
Frédéric Servais <fservais@he2b.be>
2018-09-16 12:55:46 +0200

    labo-java - draft TD10 tableaux

---
+++ labo-java/labo-java-10-tableaux/code/Tableau.java
@@ -0,0 +1,34 @@
+package esi.dev1.td10;
+
+public class Tableau {
+
+
+    public static void main(String[] args) {
+        int[] tab = {1, 2, 3, 4, 5};
+        System.out.println("taille: "+tab.length);
+        System.out.println("1er élément: "+tab[0]);
+        System.out.println("dernier élément: "+tab[tab.length-1]);
+        System.out.println("autre élément: "+tab[2]);
+        System.out.println("affiche un drôle de truc: "+tab);
+        
+        afficherTab(tab);
+        incrémenter(tab);
+        afficherTab(tab);
+        
+        int[] tab2 = new int[10];
+        afficherTab(tab2);
+    }
+    
+    static void afficherTab(int[] tab) {
+        for (int valeur : tab) {
+            System.out.print(valeur+" ");
+        }
+        System.out.println(); // on passe à la ligne
+    }
+
+    static void incrémenter(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            tab[i]++;
+        }
+    }
+}
