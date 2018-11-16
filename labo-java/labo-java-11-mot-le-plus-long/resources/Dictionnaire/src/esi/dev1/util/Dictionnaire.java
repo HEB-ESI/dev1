fa3f9a40404d2c76a761a83789f3378893f8c7ba
fservais1 <fservais@he2b.be>
2018-11-16 13:35:27 +0100

    labo-java - TD11 retour dna-nvs

--- labo-java/labo-java-11-mot-le-plus-long/resources/Dictionnaire/src/esi/dev1/util/Dictionnaire.java
+++ labo-java/labo-java-11-mot-le-plus-long/resources/Dictionnaire/src/esi/dev1/util/Dictionnaire.java
@@ -9,31 +9,44 @@ import java.util.Scanner;
  */
 public class Dictionnaire {
 
+    private static final String[] DICO;
+    // rem. : final en java porte sur la référence, pas sur le contenu référencé
+    // => il est possible de mofifier le contenu du tableau, mais
+    //    pas de référencer un autre tableau :
+    //      DICO[0] = "33"; est possible
+    //      DICO = new String[3]; ne compile pas
+
+    static {
+        Scanner input = new Scanner(Dictionnaire.class
+                .getResourceAsStream("/resources/dico.txt"));
+
+        //init DICO
+        DICO = new String[input.nextInt()];
+
+        // remplir DICO
+        int i = 0;
+        while (input.hasNext()) {
+            DICO[i++] = input.next();
+        }
+        
+        input.close();
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("10 mots au hasard: ");
         for (int i = 0; i < 10; i++) {
-            System.out.println(mots()[(int)(Math.random()*59232)]);
+            System.out.println(mots()[(int) (Math.random() * 59232)]);
         }
     }
 
     public static String[] mots() {
-        Scanner clavier = null;
-        InputStream in = Dictionnaire.class.getResourceAsStream("/resources/dico.txt");
-        clavier = new Scanner(in);
-
-        //init dico
-        int nbwords = clavier.nextInt();
-        String[] dico = new String[nbwords];
-
-        // remplir le dico
-        int i = 0;
-        while (clavier.hasNext()) {
-            dico[i] = clavier.next();
-            i++;
-        }
-        return dico;
+        return DICO;
+        // attention : le contenu du dictionnaire est modifiable...
+        // pour l'empêcher, il faut cloner le tableau, ce que je
+        // ne veux surtout pas, ou se détourner des vieux tableaux
+        // https://stackoverflow.com/a/14809392
     }
 }
